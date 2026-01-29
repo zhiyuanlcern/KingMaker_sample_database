3656e78f53d6dc0c8e613995adad70c4bf167525
zhiyuanl <zhiyuanl@cern.ch>
2026-01-29 08:16:54 +0100

    upload data list for running V14

---
+++ run_zhiyuan_syst_tt.sh
@@ -0,0 +1,20 @@
+
+replacement="shifts = [\"All\"]"
+sed -i '/^shifts = /c\'"$replacement" "lawluigi_configs/KingMaker_lxplus_luigi.cfg"
+replacement="files_per_task = 3"
+sed -i '/^files_per_task = /c\'"$replacement" "lawluigi_configs/KingMaker_lxplus_luigi.cfg"
+
+replacement="scopes = [\"tt\"]"
+sed -i '/^scopes = /c\'"$replacement" "lawluigi_configs/KingMaker_lxplus_luigi.cfg"
+
+sleep 5
+
+law run ProduceSamples --local-scheduler False --analysis tau  --config config_fullsyst --sample-list sample_database/Htautau_input_list/NanoV12_2022MC_all.txt  --workers 1 --production-tag NanoV14_2022MC_all_Version13_syst_tt > KingMaker_logs/NanoV12_2022MC_all_Version13_syst.log & 
+# python3 scripts/ProductionStatus.py --analysis tau --config config_fullsyst --sample-list sample_database/Htautau_input_list/NanoV12_2023MC_all.txt --production-tag NanoV14_2023MC_all_Version13_syst_tt &
+sleep 500
+replacement="scopes = [\"tt\"]"
+sed -i '/^scopes = /c\'"$replacement" "lawluigi_configs/KingMaker_lxplus_luigi.cfg"
+law run ProduceSamples --local-scheduler False --analysis tau  --config config_fullsyst --sample-list sample_database/Htautau_input_list/NanoV12_2023MC_all.txt  --workers 1 --production-tag NanoV14_2023MC_all_Version13_syst_tt > KingMaker_logs/NanoV12_2023MC_all_Version13_syst.log & 
+# python3 scripts/ProductionStatus.py --analysis tau --config config_fullsyst --sample-list sample_database/Htautau_input_list/NanoV12_2022MC_all.txt --production-tag NanoV14_2022MC_all_Version13_syst_tt &
+
+
